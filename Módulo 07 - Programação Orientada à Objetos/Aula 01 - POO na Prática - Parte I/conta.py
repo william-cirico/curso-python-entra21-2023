d00c0ab5a520342f1390d91561104435e879ed88
William Círico <contato.williamc@gmail.com>
2023-07-14 17:25:27 -0300

    feat(oop): :sparkles: add oop class 01 - introduction to oop
    Como criar classes, atributos, métodos e encapsulamento

---
+++ Módulo 07 - Programação Orientada à Objetos/Aula 01 - POO na Prática - Parte I/conta.py
@@ -0,0 +1,108 @@
+"""Implementação da classe Conta"""
+# É necessário para fazer a tipagem de uma classe em um método da própria classe
+from __future__ import annotations
+
+class Conta:
+    """Conta representa uma conta bancária
+    
+    Attributes:
+        numero (str): Número identificador da conta.
+        titular (str): Nome do títular da conta.
+        saldo (float): Saldo da conta.
+        limite (float): Limite da conta.
+    """
+
+    def __init__(self, numero: int, titular: str) -> None:
+        """
+        Args:
+            numero (int): Número da conta.
+            titular (str): Títular da conta.
+        
+        Raises:
+            TypeError: Se `número` não for inteiro.
+            AttributeError: Se `número` não possuir 9 dígitos.
+        """
+        if not isinstance(numero, int):
+            raise TypeError("número da conta precisa ser inteiro")
+
+        numero = str(numero)
+
+        if len(numero) != 9:
+            raise AttributeError("número da conta deve possuir 9 dígitos")
+        
+        self.numero = f"{numero[:9]}-{numero[8]}"
+
+        # Encapsulamento
+        self.titular = titular
+        self.__limite = 100
+        self.__saldo = 0
+
+    @property
+    def saldo(self) -> float: # Getter
+        """float: Saldo da conta."""
+        return self.__saldo
+    
+    @property
+    def limite(self) -> float: # Getter
+        """float: Limite da conta."""
+        return self.__limite
+
+    @property
+    def titular(self) -> str: # Getter
+        """str: Nome do titular da conta."""
+        return self.__titular
+    
+    @titular.setter
+    def titular(self, novo_titular: str) -> None: # Setter
+        self.__titular = novo_titular.title()
+
+    def depositar(self, valor: float) -> None:
+        """Deposita um valor no saldo da conta.
+        
+        Args:
+            valor: Valor do depósito.
+        """
+        self.__saldo += valor
+
+    def sacar(self, valor: float) -> bool:
+        """Saca um valor da conta se o saldo + limite for maior ou igual ao valor de saque.
+        
+        Args:
+            valor: Valor do saque.
+
+        Returns:
+            True se for bem-sucedido, False caso contrário.
+        """
+        if (self.__saldo + self.__limite) < valor:
+            print("Saldo indisponível para realizar a operação!")
+            return False
+
+        if self.__saldo < valor:
+            self.__limite -= valor - self.saldo
+            self.__saldo = 0
+        else:
+            self.__saldo -= valor
+
+        return True
+
+    def transferir(self, valor: float, conta_destino: Conta) -> None:
+        """Transfere o valor de uma conta para outra se o (saldo + limite) 
+        for maior ou igual ao valor de saque.
+
+        Args:
+            valor: Valor da trasferência.
+            conta_destino: Conta de destino da trasferência.
+        """
+        if (self.sacar(valor)):
+            conta_destino.depositar(valor)
+            print("Trasferência realizada com sucesso!")
+
+    def exibir_extrato(self):
+        """Exibe o extrato da conta na tela."""
+        print(f"Número da conta: {self.numero}.\nSaldo: {self.saldo:.2f}.\nLimite: {self.__limite:.2f}")
+
+
+if __name__ == "__main__":
+    conta = Conta(123456789, "William")
+    conta.depositar(20)
+    conta.exibir_extrato()
