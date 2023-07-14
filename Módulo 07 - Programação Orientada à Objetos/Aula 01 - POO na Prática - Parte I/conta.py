04813bbf690e3ba6416105537abc7daf1bf589d3
William Círico <contato.williamc@gmail.com>
2023-07-14 17:39:38 -0300

    feat(oop): :sparkles: add static and class methods
    Adicionando exemplo de métodos estáticos e métodos de classe

--- Módulo 07 - Programação Orientada à Objetos/Aula 01 - POO na Prática - Parte I/conta.py
+++ Módulo 07 - Programação Orientada à Objetos/Aula 01 - POO na Prática - Parte I/conta.py
@@ -11,6 +11,7 @@ class Conta:
         saldo (float): Saldo da conta.
         limite (float): Limite da conta.
     """
+    __quantidade_contas = 0 # Atributo estático
 
     def __init__(self, numero: int, titular: str) -> None:
         """
@@ -37,6 +38,8 @@ class Conta:
         self.__limite = 100
         self.__saldo = 0
 
+        Conta.__quantidade_contas += 1
+
     @property
     def saldo(self) -> float: # Getter
         """float: Saldo da conta."""
@@ -55,6 +58,16 @@ class Conta:
     @titular.setter
     def titular(self, novo_titular: str) -> None: # Setter
         self.__titular = novo_titular.title()
+
+    @classmethod
+    def quantidade_contas(cls) -> int:
+        """Retorna a quantidade de contas criadas através da classe."""
+        return cls.__quantidade_contas
+    
+    @staticmethod
+    def numero_conta_valido(numero: int) -> bool:
+        """Verifica se o número da conta é valido"""
+        return not isinstance(numero, int) or numero != 9
 
     def depositar(self, valor: float) -> None:
         """Deposita um valor no saldo da conta.
@@ -106,3 +119,10 @@ if __name__ == "__main__":
     conta = Conta(123456789, "William")
     conta.depositar(20)
     conta.exibir_extrato()
+    print(f"Quantidade de contas criadas: {Conta.quantidade_contas()}")
+
+    NUMERO_CONTA = 12345678
+    if Conta.numero_conta_valido(NUMERO_CONTA):
+        print(f"O número {NUMERO_CONTA} é válido")
+    else:
+        print(f"O número {NUMERO_CONTA} é inválido")
