39d60443e4da2bdbeb076eb214335392e3536b62
William Círico <william.cirico@proway.com.br>
2023-09-26 14:13:05 -0300

    feat(html): :sparkles: add bootstrap lesson

---
+++ Módulo 04 - HTML e CSS/Aula 06 - Bootstrap/js/main.js
@@ -0,0 +1,3 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    
