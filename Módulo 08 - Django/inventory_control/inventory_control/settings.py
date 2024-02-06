5967b84400c567a9c961d087bf6ee1f836debf92
William Círico <william.cirico@proway.com.br>
2024-02-06 14:21:50 -0300

    removing folders

--- Módulo 08 - Django/inventory_control/inventory_control/settings.py
+++ Módulo 08 - Django/inventory_control/inventory_control/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
