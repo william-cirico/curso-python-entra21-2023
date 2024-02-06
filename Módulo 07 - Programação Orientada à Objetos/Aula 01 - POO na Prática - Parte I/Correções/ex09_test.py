0785b3371cad4b0f70a2022ff3f9a404a31648dc
William Círico <william.cirico@proway.com.br>
2024-02-06 14:29:03 -0300

    feat: :sparkles: preparing for deploy

--- Módulo 07 - Programação Orientada à Objetos/Aula 01 - POO na Prática - Parte I/Correções/ex09_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from ex09 import Person
-
-def test_get_total_people():
-    person1 = Person("Person 1", 22)
-    person2 = Person("Person 2", 23)
-    
-    assert Person.get_total_people() == 2
